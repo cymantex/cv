16f807e33ee392a344ea30c2c2af25833ac49b92
dv16sen <dv16sen@cs.umu.se>
2019-01-07 14:53:54 +0100

    Added basic project structure

---
+++ src/Cv.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Cv = () => (
+    <h1>Cv</h1>
+);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {render} from "react-dom";
+import "./sass/style.scss";
+import {Cv} from "./Cv";
+
+render(<Cv/>, document.getElementById("root"));
